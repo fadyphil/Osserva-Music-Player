5b7cc2b8da76a5a3ae1b31dcc272d678c69f5fb5
fadyphil <fadyph2003@gmail.com>
2026-03-16 20:27:55 +0200

    chore(ios): update generated Flutter environment and plugin registrant
    - Updated paths and dependencies for release environment

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/fady/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/fady/Projects/CURRENT/music_player"
+export "FLUTTER_APPLICATION_PATH=/home/fady/Projects/CURRENT/a-personal/music-player"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
